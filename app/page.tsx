4bef00cc185c649c88ff780a3df4fb15034ee94c
myesn <myesn@foxmail.com>
2024-09-12 11:01:19 +0800

    引入 alert-dialog 和 demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { AlertDialogDemo } from '@/components/demo/alert-dialog-demo'
+
 export default function Home() {
   return (
     <div>
       hi
+      <AlertDialogDemo />
     </div>
   );
 }
