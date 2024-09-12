5f09ec8124c3a8a34eabf9b2dccd598c55634150
myesn <myesn@foxmail.com>
2024-09-12 11:05:50 +0800

    引入 mode-toggle demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { AlertDialogDemo } from '@/components/demo/alert-dialog-demo'
+import { ModeToggleDemo } from '@/components/demo/mode-toggle-demo'
 
 export default function Home() {
   return (
     <div>
       hi
       <AlertDialogDemo />
+      <ModeToggleDemo />
     </div>
   );
 }
